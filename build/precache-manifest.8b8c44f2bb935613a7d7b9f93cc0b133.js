4b9cf2de1453dfa2404fe4bf238c2782bfc47c58
Fletcher LaRue <laruefletcher@gmail.com>
2019-01-29 16:27:15 -0800

    adds build

---
+++ build/precache-manifest.8b8c44f2bb935613a7d7b9f93cc0b133.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "87bed31ae43a591fbc82",
+    "url": "/static/js/main.87bed31a.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "87bed31ae43a591fbc82",
+    "url": "/static/css/main.17ffbdbf.chunk.css"
+  },
+  {
+    "revision": "0a77f00631847570f59d391301b67f26",
+    "url": "/index.html"
+  }
+];
